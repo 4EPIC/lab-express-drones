16d1d1c47325a646c2fd3c6a378618d0bbb279d9
Eric Esteban <eric_7@hotmail.com>
2021-11-15 10:50:43 +0100

    done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,10 +1,10 @@
 // Iteration #1
 const mongoose = require("mongoose");
-const Drone = require("./../models/Drone.model");
+const Drone = require("../models/Drone.model");
 
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
